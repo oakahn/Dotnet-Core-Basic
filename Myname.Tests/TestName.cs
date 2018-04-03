4637af33dc5516ca094229c189c2edfa8534f30d
Todsphol <theaddza11@gmail.com>
2018-04-03 15:45:44 +0700

    Create new test case

--- Myname.Tests/TestName.cs
+++ Myname.Tests/TestName.cs
@@ -5,13 +5,41 @@ namespace Todsphol.Myname.Tests
 {
     public class TestName
     {
+        private Name myName;
+        public TestName() {
+            myName = new Name();
+        }
         [Fact]
-        public void TestGetNameShouldReturnMyName()
+        public void TestGetNameWithoutLangugeShouldReturnMyNameInEnglish()
         {
-            Name myName = new Name();
             string result = myName.Get();
 
             Assert.Equal("Todsphol", result);
         }
+
+        [Fact]
+        public void TestGetnameWithEnglishLanguageShouldReturnMyNameInEnglish() 
+        {
+            string result = myName.Get("e");
+            Assert.Equal("Todsphol", result);
+        }
+
+        [Fact]
+        public void TestGetNameWithThaiLanguageShoudReturnMyNameInThai() 
+        {
+            string result = myName.Get("t");
+            Assert.Equal("ทศพล", result);
+        }
+
+        [Theory]
+        [InlineData ("e", "Todsphol")]
+        [InlineData ("t", "ทศพล")]
+
+        public void TestGetNameWithLanguageShouldRerurnMyNameInGivenLanguage(string inputLanguage, string expectedResult) 
+        {
+            string result = myName.Get(inputLanguage);
+            Assert.Equal(expectedResult, result);
+        }
     }
+
 }
--- Myname/Name.cs
+++ Myname/Name.cs
@@ -4,8 +4,21 @@ namespace Todsphol.Myname
 {
     public class Name
     {
-       public string Get() {
+       public string Get(string lang = "e") {
+           if ("e".Equals(lang)) {
+               return GetEnglish();
+           }
+            return GetThai();
+        }
+
+        private string GetEnglish()
+        {
             return "Todsphol";
         }
+
+        public string GetThai() {
+            return "ทศพล";
+        }
+
     }
 }
