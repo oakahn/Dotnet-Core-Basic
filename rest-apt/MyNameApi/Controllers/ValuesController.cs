d7615fd93a8ddd8b4fba78efe5723569c4d3c4ae
Todsphol <theaddza11@gmail.com>
2018-04-03 11:44:03 +0700

    rest-api add reference Myname.csproj

--- rest-apt/MyNameApi/Controllers/ValuesController.cs
+++ rest-apt/MyNameApi/Controllers/ValuesController.cs
@@ -3,42 +3,23 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using System.Dynamic;
 
 namespace MyNameApi.Controllers
 {
+    
     [Route("api/[controller]")]
-    public class ValuesController : Controller
+    public class NameController : Controller
     {
-        // GET api/values
+        // GET api/name
         [HttpGet]
-        public IEnumerable<string> Get()
+        public IActionResult Get()
         {
-            return new string[] { "value1", "value2" };
+             dynamic obj = new ExpandoObject();
+            obj.name = "Watch me";
+            return Ok(obj);
+
         }
 
-        // GET api/values/5
-        [HttpGet("{id}")]
-        public string Get(int id)
-        {
-            return "value";
-        }
-
-        // POST api/values
-        [HttpPost]
-        public void Post([FromBody]string value)
-        {
-        }
-
-        // PUT api/values/5
-        [HttpPut("{id}")]
-        public void Put(int id, [FromBody]string value)
-        {
-        }
-
-        // DELETE api/values/5
-        [HttpDelete("{id}")]
-        public void Delete(int id)
-        {
-        }
     }
 }
